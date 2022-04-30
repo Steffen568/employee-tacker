e44b10304fadb869569ff44a227c2c858b998142
Steffen568 <sgonzalez568@gmail.com>
2022-04-30 14:00:23 -0500

    mainMenue complete

---
+++ index.js
@@ -0,0 +1,71 @@
+const inquirer = require('inquirer')
+const mysql = require('mysql2')
+const conTable = require('console.table')
+const db = require('./config/connection')
+
+
+// Connect to sql database, then promp main menu
+db.connect(err => {
+    if(err) throw err
+    console.log(`
+    ==============================
+    CONNECTED TO EMPLOYEE DATABASE
+    ==============================
+    `)
+    mainMenue()
+})
+
+const mainMenue = () => {
+    inquirer.prompt({
+        type: 'list',
+        name: 'mainMenu',
+        message: "SELECT FROM THE MENU BELOW",
+        choices: [
+            'View all Departments',
+            'View all Positions',
+            'View all Employees',
+            'Add Department to database',
+            'Add Position to database',
+            'Add Employee to database',
+            'Updata Employee',
+            'Exit'
+        ]
+    })
+        .then(response => {
+            switch (response.mainMenu) {
+                case 'View all Departments':
+                    departments()
+                    break
+                case 'View all Positions':
+                    positions()
+                    break
+                case 'View all Employees':
+                employees()
+                break
+                case 'Add Department to database':
+                    addDepartments()
+                    break
+                case 'Add Position to database':
+                    addPosition()
+                    break
+                case 'Add Employee to database':
+                    addEmployee()
+                    break
+                case 'Update Employee':
+                    updateEmployee()
+                    break
+                case 'Exit':
+                    db.end(err => {
+                        if (err) {
+                            return console.log(err)
+                        }
+                        console.log(`
+                            ========================
+                                SESSION HAS ENDED
+                            ========================    
+                        `)
+                    })
+            }
+        })
+}
+
